b75ba54c0a87e436fb569ed42411b5fad123f687
mikuchievaa <milanakuchievaa@mail.ru>
2025-10-17 19:03:06 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -4,31 +4,47 @@ import (
 	"fmt"
 )
 
-func main(){
-	user1 := Reader{
-		ID: 1,
-		FirstName: "Ilona",
-		LastName: "Valieva",
-		IsActive: true,
-	}
-
-	fmt.Println(user1)
-	user1.DisplayReader()
-	user1.Deactivate()
-	user1.Activate()
-	fmt.Println(user1)
-
-
-	notifiers := []Notifier{}
-	EmailNotifier:= EmailNotifier{EmailAddress: "ilonava09@gmail.com"}
-	SMSNotifier:=SMSNotifier{PhoneNumber: "+79163158170"}
-
-	notifiers=append(notifiers, EmailNotifier,SMSNotifier)
-	message:="Ваша книга просрочена!"
-
-	for _, notifier:=range notifiers{
-		notifier.Notify(message)
-	}
+type Storable interface{
+	Save() error
+	Load() error
 }
+
+
+func main(){
+	myLibrary := Library{
+		Books: []*Book{
+			{ID:1,Title: "Как исключить надоедливого студента 2",Author:"Газзаев Батраз",Year:3892,IsIssued:false},
+			{ID:2,Title: "Биография Юлия Цезаря ",Author:"Гобозов Богдан",Year:2025,IsIssued:false},
+			{ID:3,Title: "1000 и 1 отмазка для прогуливания пар",Author:"Газзаев Батраз и Гобозов Богдан",Year:2002,IsIssued:false},
+
+		},
+		Readers: []*Reader{
+			{ID:1,FirstName: "Илона",LastName: "Валиева",IsActive: false},
+			{ID:2,FirstName: "Милана",LastName: "Кучиева",IsActive: false},
+			{ID:3,FirstName: "Марлен",LastName: "Албегов",IsActive: false},
+
+		},
+	}
+
+
+
+	fmt.Println("---Тестируем выдачу книг---")
+	//Выдаем книгу 1 читателю 1
+	err := myLibrary.IssueBookToReader(1, 1)
+	if err != nil {
+	fmt.Println("Ошибка выдачи", err)
+	}
+	//Проверить статус книги после выдачи
+	book, _ := myLibrary.FindBookByID(1)
+	if book != nil {
+	fmt.Println("Статус книги после выдачи:", book)
+	}
+	//Попытка выдать несуществующую книгу
+	err = myLibrary.IssueBookToReader(99, 1)
+	if err != nil {
+	fmt.Println("Ожидаемая ошибка:", err)
+	}
+}	
+
 
 
--- models.go
+++ models.go
@@ -1,72 +1,166 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Book struct {
+	ID       int
+	Title    string
+	Author   string
+	Year     int
+	IsIssued bool
+	ReaderID *int //ID читателя, который взял книгу
+}
+
+// IssueBook выдает книгу читателю
+func (b *Book) IssueBook(reader *Reader) {
+	if b.IsIssued {
+		fmt.Printf("Книга '%s' уже кому-то выдана\n", b.Title)
+		return
+	}
+	if !reader.IsActive {
+		fmt.Printf("Читатель %s %s не активен и не может получить книгу.", reader.FirstName, reader.LastName)
+		return
+	}
+	b.IsIssued = true
+	b.ReaderID = &reader.ID
+	fmt.Printf("Книга '%s' была выдана читателю %s %s\n", b.Title, reader.FirstName, reader.LastName)
+}
+
+// ReturnBook возвращает книгу в библиотеку
+func (b *Book) ReturnBook() {
+	//Нужно будет реализовать с учетом нового в проекте
+	if !b.IsIssued {
+		fmt.Printf("Книга '%s' и так в библиотеке", b.Title)
+		return
+	}
+	b.IsIssued = false
+	b.ReaderID = nil
+	fmt.Printf("Книга '%s' возвращена в библиотеку\n", b.Title)
+}
 
 type Reader struct {
 	ID        int
 	FirstName string
 	LastName  string
 	IsActive  bool
-
-}
-type Book struct {
-	ID       int
-	Title    string
-	Author   string
-	Year int
-	IsIssued bool
-	ReaderId *int
 }
 
-func (r Reader) DisplayReader() {
-	fmt.Printf("Читатель: %s %s (ID: %d)\n", r.FirstName , r.LastName, r.ID)
-}
+// DisplayReader выводит полную информацию о пользователе
+//Этот метод больше не нужен, потому что мы реализовали String() для Reader
+/*func (r Reader) DisplayReader() {
+	fmt.Printf("Читатель: %s %s (ID: %d)\n", r.FirstName, r.LastName, r.ID)
+}*/
 
-func (r *Reader) Deactivate() {
-	r.IsActive = false
-	fmt.Printf("Пользователь %s %s деактивирован", r.FirstName, r.LastName)
-}
-
-
-func (r *Reader) Activate() {
-	r.IsActive = true
-	fmt.Printf("Пользователь %s %s активирован", r.FirstName, r.LastName)
-}
-
-func(r *Reader) String()string{
+func (r Reader) String() string {
 	status := ""
-	if r.IsActive{
+	if r.IsActive {
 		status = "активен"
-	}else{
+	} else {
 		status = "не активен"
 	}
-	return fmt.Sprintf("Пользователь: %s %s  (ID:%d, $s\n)", r.FirstName,r.LastName,r.ID,status)
+	return fmt.Sprintf("Пользователь %s %s, № %d, пользователь %s", r.FirstName, r.LastName, r.ID, status)
+}
+
+// Deactivate делает пользователя неактивным
+func (r *Reader) Deactivate() {
+	r.IsActive = false
 }
 
 func (b Book) String() string {
-	return fmt.Sprintf("%s (%s, %d)", b.Title, b.Author, b.Year)
+	status := "в библиотеке"
+	if b.IsIssued && b.ReaderID != nil {
+		status = fmt.Sprintf("на руках у читателя с ID %d", *b.ReaderID)
+	}
+	
+	return fmt.Sprintf("%s (%s, %d), статус %s", b.Title, b.Author, b.Year, status)
 }
 
-func (b *Book) IssueBook(r *Reader){
-	if b.IsIssued{
-		fmt.Printf(" Книга %s уже выдана другому посетителю библиотеки, выберите другую\n", b.Title)
-		return
-	}
-	// if !r.IsActive{
-	// 	fmt.Printf(" Пользователь неактивен, книга %s не может быть взята\n", b.Title)
-	// 	return
-	// }
-	b.IsIssued = true
-	b.ReaderId = &r.ID
-	fmt.Printf("Книга %s выдана: %s %s\n", b.Title, r.FirstName, r.LastName )
+//Центральная структура-агрегатор
+type Library struct {
+	Books   []*Book
+	Readers []*Reader
+
+	//Счетчики для генерации ID
+	lastBookID   int
+	lastReaderID int
 }
 
-func (b *Book) ReturnBook(){
-	if !b.IsIssued{
-		fmt.Printf("Книга %s ни у кого не находится\n", b.Title)
-		return
+func (lib *Library) AddReader(firstName, lastName string) *Reader {
+	lib.lastReaderID++
+
+	//Создаем нового читателя
+	newReader := &Reader{
+		ID:        lib.lastReaderID,
+		FirstName: firstName,
+		LastName:  lastName,
+		IsActive:  true, //Новый читатель всегда активный
 	}
-	b.IsIssued = false
-	b.ReaderId = nil 
-	fmt.Printf("Книга %s возвращена\n", b.Title)
+
+	//Добавляем читателя в срез
+	lib.Readers = append(lib.Readers, newReader)
+
+	fmt.Printf("Зарегистрирован новый читатель: %s %s \n", firstName, lastName)
+	return newReader
+}
+
+// Добавляем новую книгу в библиотеку
+func (lib *Library) AddBook(title, author string, year int) *Book {
+	lib.lastBookID++
+
+	//Создаем новую книгу
+	newBook := &Book{
+		ID:       lib.lastBookID,
+		Title:    title,
+		Author:   author,
+		Year:     year,
+		IsIssued: false, //Новая книга всегда в наличии
+	}
+
+	//Добавляем новую книгу в библиотеку
+	lib.Books = append(lib.Books, newBook)
+
+	fmt.Printf("Добавлена новая книга: %s\n", newBook)
+	return newBook
+}
+
+// Поиск книгу по  ID
+func (lib *Library) FindBookByID(id int) (*Book, error) {
+	for _, book := range lib.Books {
+		if book.ID == id {
+			return book, nil
+		}
+	}
+
+	return nil, fmt.Errorf("книга с ID %d не найдена в библиотеке", id)
+}
+
+// Поиск читателя по ID
+func (lib *Library) FindReaderByID(id int) (*Reader, error) {
+	for _, reader := range lib.Readers {
+		if reader.ID == id {
+			return reader, nil
+		}
+	}
+
+	return nil, fmt.Errorf("читатель с ID %d не найден", id)
+}
+
+// Метод для выдачи книги читателю
+func (lib *Library) IssueBookToReader(bookID, readerID int) error {
+	book, err := lib.FindBookByID(bookID)
+	if err != nil {
+		return err
+	}
+
+	reader, err := lib.FindReaderByID(readerID)
+	if err != nil {
+		return err
+	}
+
+	//Выдаем книгу с помощью метода IssueBook
+	book.IssueBook(reader)
+	return nil
+
 }
